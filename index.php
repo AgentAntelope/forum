03b5db202547ef404a3eff7d0c464d359cc17963
Arif Arman <mylo.xyloto53@gmail.com>
2013-12-10 09:48:32 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
-    <title>The Ultimate Forum</title>
+    <title>The Weird Thing</title>
     <head>
         <a href="secondPage.php" target="_blank">Let's Jump To Second Page</a>
     </head>
