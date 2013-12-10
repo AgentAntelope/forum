d8d13c6ac272bd43ad3a2480fe8b351f99f59f37
zitu <mylo.xyloto53@gmail.com>
2013-12-10 09:35:30 +0600

    First draft

---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+    <title>The Ultimate Forum</title>
+    <head>
+        <a href="secondPage.php" target="_blank">Let's Jump To Second Page</a>
+    </head>
+</html>
---
+++ secondPage.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <meta name="Zitu" content="Bla bla">
+        <!--<meta http-equiv="refresh" content="5">-->
+        <title>Second Page</title>
+        <h1>Was refreshing every 5 seconds</h1>
+        <h1>Now showing a nigga</h1>
+    </head>
+    <body>
+        <img src="zitu.jpg" alt="Nigga" height="320" width="480">
+        <br>
+        <table border="1">
+            <tr>
+                <th>Name</th>
+                <th>ID</th>
+            </tr>
+            <tr>
+                <td>Zitu</td>
+                <td>46</td>
+            </tr>
+            <tr>
+                <td>Maw</td>
+                <td>53</td>
+            </tr>
+        </table>
+    </body>
+</html>
