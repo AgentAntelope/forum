ec4378012e1beed18b3971301c0b996128636a32
Agent Antelope <agentantelope@gmail.com>
2014-03-22 16:06:57 +0000

    Update secondPage.php

--- secondPage.php
+++ secondPage.php
@@ -4,10 +4,10 @@
         <!--<meta http-equiv="refresh" content="5">-->
         <title>Second Page</title>
         <h1>Was refreshing every 5 seconds</h1>
-        <h1>Now showing a nigga</h1>
+        <h1>Now showing some fine gentleman</h1>
     </head>
     <body>
-        <img src="zitu.jpg" alt="Nigga" height="320" width="480">
+        <img src="zitu.jpg" alt="gentleman" height="320" width="480">
         <br>
         <table border="1">
             <tr>
